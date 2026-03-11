1cab59b4d8b2723e6b145e0b1f135e0268780f87
Graia Ramin <142338131+Ramine92@users.noreply.github.com>
2026-03-11 19:48:09 +0100

    fixed url for api

--- app/core/config.py
+++ app/core/config.py
@@ -5,5 +5,6 @@ BASE_DIR = Path(__file__).parent.parent.parent
 load_dotenv(BASE_DIR/ ".env")
 MODEL_NAME = os.getenv("MODEL_NAME","LinearRegression")
 MODEL_VERSION = "v1"
-API_URL = "localhost:8000"
+production = True
+API_URL = "localhost:8000" if production == False else "https://ames-housing-mlops.onrender.com"
 MODEL_PATH = BASE_DIR / "ml" / "models" / "artifacts" / f"{MODEL_NAME}_{MODEL_VERSION}.pkl"
